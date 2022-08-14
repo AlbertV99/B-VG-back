dfdc9b8990031371e8149a2038e2c8a2b3b9ffc8
Alberto Valdez <tico.urquhart@gmail.com>
2022-08-14 00:05:11 -0400

    Creacion de la tabla parametros

---
+++ database/migrations/2022_08_14_035802_create_parametros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParametrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parametros', function (Blueprint $table) {
+            $table->id();
+            $table->string("direccion_mail_saliente");
+            $table->string("servidor_mail");
+            $table->string("puerto_mail");
+            $table->string("interes_bcp");
+            $table->string("interes_moratorio");
+            $table->string("interes_punitorio");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parametros');
+    }
+}
